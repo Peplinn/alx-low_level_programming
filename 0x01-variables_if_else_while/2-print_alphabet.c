bd6d9e706199873cc3d9a4b51f12803de5c694d3
Peplinn <chidiebubeoluoma01@gmail.com>
2023-06-16 19:30:32 -0700

    0-3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+char letter;
+for (ch == 'a'; ch <= 'z'; ch++)
+{       
+putchar(ch);
+}       
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
