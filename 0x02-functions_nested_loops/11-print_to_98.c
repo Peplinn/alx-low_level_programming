5be78f7761b2c3e5d0f5d643a9854332a8931d52
Peplinn <chidiebubeoluoma01@gmail.com>
2023-06-21 06:17:54 -0700

    Betty Doc

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,18 @@
 void print_to_98(int n)
 {
 int i = n;
-if (i <= 98) {
-while (i < 98) {
+if (i <= 98)
+{
+while (i < 98)
+{
 printf("%d, ", i);
 i++;
 }
 }
-else if (i >= 98) {
-while (i > 98) {
+else if (i >= 98)
+{
+while (i > 98)
+{
 printf("%d, ", i);
 i--;
 }
