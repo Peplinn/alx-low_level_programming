ef03b86c2cdff6a78a76d9dee92b04d5bd7c93a0
Peplinn <chidiebubeoluoma01@gmail.com>
2023-06-20 20:57:21 -0700

    0,1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all letters in lowercase
+ * prints _putchar
+ * 
+ */
+
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');
+}
+
