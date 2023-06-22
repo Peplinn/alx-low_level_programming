c8368c3b00ce4686052ea6f26b683917d7b92638
Peplinn <chidiebubeoluoma01@gmail.com>
2023-06-22 13:39:22 -0700

    Initial Commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - multiplies two integers
+*
+* Return: the result of the multiplication
+*/
+void print_numbers(void)
+{
+int n;
+n = 0
+while (n < 10)
+{
+_putchar(n);
+n++
+}
+putchar('\n');
+}
