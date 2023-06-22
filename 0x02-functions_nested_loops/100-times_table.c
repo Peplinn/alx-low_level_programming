778d96f2707527190ea980ae194ca39a541d0e0a
Peplinn <chidiebubeoluoma01@gmail.com>
2023-06-21 21:58:01 -0700

    ssa

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* times_table - prints times table 0-n
+* print_times_table - prints times table 0-n
 * each line ends with newline
 * @n: print to this number
 */
 
-void times_table(void)
+void print_times_table(int n)
 {
 int i, j, c;
 for (i = 0; i <= n; i++)
