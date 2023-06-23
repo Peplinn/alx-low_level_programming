24f667e3f23ce4e5938c030d2bba8b2249d8214f
Peplinn <100541154+Peplinn@users.noreply.github.com>
2023-06-23 03:21:25 +0000

    Prnt most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_most_numbers - prints 1 - 9 w/o 2,4
+*/
+
+void print_most_numbers(void)
+{
+int n;
+n = 0;
+while (n < 10)
+{
+if (n == 2 || n == 4)
+continue
+else
+{
+_putchar(n);
+n++;
+}
+}
+_putchar('\n');
+}
