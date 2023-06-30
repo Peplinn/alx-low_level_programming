389306c00a53b356dc5292b5f13cad82332f4cd9
Peplinn <100541154+Peplinn@users.noreply.github.com>
2023-06-30 01:14:21 +0000

    Initial Commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strcat - Appends string at src to that at dest
+* @dest: Pointer to destination string
+* @src: Pointer to source string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+while (*dest != '\0') {
+dest++;
+}
+
+while (*src != '\0') {
+dest = src;
+dest++;
+src++;
+}
+
+dest = '\0';
+}
