3122d2c2ad18e5265622c08db42373f06e1d2b18
Peplinn <100541154+Peplinn@users.noreply.github.com>
2023-06-30 02:25:18 +0000

    Created _strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* _strncpy - Copies str at src to dest
+* @dest: Pointer to destination string
+* @src: Pointer to source string
+* @n: Number of chars to be copied
+* Return: originalDest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+char *originalDest = dest;
+
+while (*src && n > 0)
+{
+*dest = *src;
+*src++;
+*dest++;
+n--;
+}
+
+while (n > 0)
+{
+*dest = '\0';
+*dest++;
+n--;
+}
+
+return (originalDest);
+}
