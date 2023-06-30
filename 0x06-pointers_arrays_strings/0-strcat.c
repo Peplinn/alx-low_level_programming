1907b26b2027c17c2daab7869a4c26341fd0b609
Peplinn <100541154+Peplinn@users.noreply.github.com>
2023-06-30 01:32:35 +0000

    Fixed Betty compliance

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,11 +8,11 @@
 
 char *_strcat(char *dest, char *src)
 {
-while (*dest != '\0') {
+while (*dest != '\0')
 dest++;
-}
 
-while (*src != '\0') {
+while (*src != '\0')
+{
 *dest = *src;
 dest++;
 src++;
