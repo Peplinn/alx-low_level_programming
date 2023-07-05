ed8886d5100a7c5df70522076f7da33d423caeca
Peplinn <100541154+Peplinn@users.noreply.github.com>
2023-07-05 21:03:26 +0000

    Initial Commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strlen_recursion - prints string length
+* @s: pointer to string
+*/
+
+int c = 0;
+void _strlen_recursion(char *s)
+{
+if(*s == '\0')
+{
+putchar('0' + c);
+}else
+{
+c++;
+_puts_recursion(s+1);
+}
+}
