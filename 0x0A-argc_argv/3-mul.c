bcf17abdb296bf7b96e0a48482dcb4bdb8ed948d
Peplinn <100541154+Peplinn@users.noreply.github.com>
2023-07-08 01:03:39 +0000

    Minor fixes

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,8 +15,6 @@ if (argc != 3)
 printf("Error\n");
 return (1);
 }
-int num1 = atoi(argv[1]);
-int num2 = atoi(argv[2]);
-printf("%d\n", num1 * num2);
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 return (0);
 }
