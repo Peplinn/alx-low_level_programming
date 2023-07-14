4353e60b7f6676ae9165b086264337cc680dc98b
Peplinn <chidiebubeoluoma01@gmail.com>
2023-07-14 05:24:45 +0100

    Added realloc declaration to main

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,5 +10,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *calloc(size_t num_elements, size_t element_size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif /* MAIN_H */
