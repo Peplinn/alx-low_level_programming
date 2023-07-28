42fcb5856e9b734f409f8294166493c0c13e4dde
Peplinn <chidiebubeoluoma01@gmail.com>
2023-07-28 02:07:11 +0100

    Added strlen to files

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * add_node - Adds a node to the linked list
- * @head: Pointer to pointer to head
- * @str: Strinng to add to node
- * Return: Address of new node, else, NULL
+* add_node - Adds a node to the linked list
+* @head: Pointer to pointer to head
+* @str: Strinng to add to node
+* Return: Address of new node, else, NULL
 */
 
 list_t *add_node(list_t **head, const char *str)
@@ -30,3 +30,20 @@ new_node->next = *head;
 
 return (new_node);
 }
+
+/**
+* _strlen - Calculate the lenght of a string.
+* @str: String passed to the function.
+* Return: The lenght of a string.
+*/
+
+int _strlen(char *str)
+{
+int len;
+
+for (len = 0; str[len]; len++)
+;
+
+return (len);
+}
+
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -42,3 +42,19 @@ pos->next = new_node;
 
 return (new_node);
 }
+
+/**
+* _strlen - Calculate the lenght of a string.
+* @str: String passed to the function.
+* Return: The lenght of a string.
+*/
+
+int _strlen(char *str)
+{
+int len;
+
+for (len = 0; str[len]; len++)
+;
+
+return (len);
+}
