0b1abf9e7d584d199d463bfd79be86350a98e836
Peplinn <chidiebubeoluoma01@gmail.com>
2023-08-04 06:03:32 +0100

    Task 0 fix4

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -24,3 +24,23 @@ decimal += (b[i] + '0') * _pow_recursion(2, length - i - 1);
 
 return (decimal);
 }
+
+
+/**
+* _pow_recursion - prints exponents recursively
+* @x: number to exponent
+* @y: exponent
+* Return: -1 if exponent is negative
+* 1 if y is 0
+* else, exponent
+*/
+
+unsigned int _pow_recursion(unsigned int x, unsigned int y)
+{
+if (y < 0)
+return (-1);
+else if (y == 0)
+return (1);
+else
+return (x * _pow_recursion(x, y - 1));
+}
